5f78c8a4b4e991b32d533040233a0f67b02de091
aajc <ajarrold@ucsd.edu>
2023-01-17 12:21:47 -0800

    add UtilitiesTest

---
+++ app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
@@ -0,0 +1,17 @@
+package edu.ucsd.cse110.lab2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import androidx.test.core.app.ActivityScenario;
+
+public class UtilitiesTest {
+    @Test
+    public void test_trimDisplayStr() {
+
+        assertEquals("1.234", Utilities.trimDisplayStr("1.2340"));
+        assertEquals("13", Utilities.trimDisplayStr("13.0"));
+    }
+
+}
